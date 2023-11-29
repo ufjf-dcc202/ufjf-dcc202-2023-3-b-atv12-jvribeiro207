25c944ee54d2705476240b66219abd9718762a56
João Victor Macedo Ribeiro <joaovictor.ribeiro@estudante.ufjf.br>
2023-11-28 22:57:43 -0300

    altera index

--- estoque.js
+++ estoque.js
@@ -1,4 +1,16 @@
 let estoque = {
-    'joao':
-    'maria':
+    'joao': {
+        'maca': 1,
+        'banana' : 2
+    },
+    'maria': {
+        'maca': 2,
+        'pera': 3
+    }
 };
+
+function getEstoque(){
+    return structuredClone(estoque);
+}
+
+export {getEstoque};
